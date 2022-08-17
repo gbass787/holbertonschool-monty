a7e1371b691ce62f27dcea2b7922cccafa5cf5e6
griselizr <griseliz.r.c15@gmail.com>
2022-08-17 01:15:56 +0000

    add struct and prototypes

--- monty.h
+++ monty.h
@@ -4,7 +4,31 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdlib.h>
-#include <stderr.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ *struct monty_s - main variables of monty
+ *@file: monty file
+ *@line: line
+ *@quenue: flag to indicate if its stack or quenue mode
+ *@stack_mount: stack builing
+ *@line_count: line to be read from stack
+ */
+typedef struct monty_s
+{
+  bool quenue;
+  char *line;
+  FILE *file;
+  unsigned int line_count;
+  size_t stack_mount;
+}monty_t;
+
+#define STACK 0
+#define QUENUE 1
+
 extern char *glovar;
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -33,12 +57,13 @@ typedef struct stack_s
 typedef struct instruction_s
 {
         char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+        void (*f)(stack_t **stack, unsigned int line_count);
 } instruction_t;
 
 void push(stack_t **stack, unsigned int line_count);
 void pall(stack_t **stack, __attribute__((unused))unsigned int line_count);
 void executeop(char *opcode, stack_t **stack, unsigned int line_count);
+void _pint(stack_t **stack, unsigned int line_count)
 int getdigit(char *digit);
 unsigned int count(stack_t *stack);
 #endif
