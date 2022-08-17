bf108563f3f579d18f8c6590f18855d71bed3b06
griselizr <griseliz.r.c15@gmail.com>
2022-08-17 15:04:35 +0000

    add a new file

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop-does nothing
+ * @stack: pointer top stack
+ * @line_count - line to read
+ * Return: 0
+ */
+void nop(stack_t **stack, unsigned int line_count)
+{
+	(void) stack;
+	(void) line_number;
+}
