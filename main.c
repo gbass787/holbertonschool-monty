a4ce010c96aa7e42c8b41e0acc34139e7ef7ce1b
griselizr <griseliz.r.c15@gmail.com>
2022-08-17 23:07:15 +0000

    fix syntax error

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include "monty.h"
 
-monty_t monty;
 
 /**
  * main - monty bytecode interpreter
@@ -8,35 +7,42 @@ monty_t monty;
  * @filename: path to the file containing Monty byte code
  *Return: EXIT_SUCEEE or EXIT_FALILURE
  */
-int main(int argc, char *filename)
+int main(int argc, char **filename)
 {
+	char *line = NULL;
+	FILE *file = NULL;
+	int input = 1;
+	size_t stack_amount;
+	stack_t *stack = NULL;
+
 	if (argc != 2)
 	{
-		dprintf(STDERR_FILENO, "USAGE: monty file\n");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
-
+	}
 	file = fopen(filename, "r");
 
 	if (!file)
 	{
-		fprintf((STDERR_FILENO, "Error: Can't open file %s\n", filename);
-		fprintf(STDERR_FILENO, "Error: malloc failed\n");
+		fprintf(stderr, "Error: Can't open file %s\n", filename);
+		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
-	while (getline(&line, &stack_amount, *file) > 0)
+	line = NULL;
+	while (getline(&line, &stack_amount, file) > 0)
 	{
-		if (executeop(&stack, line_count, line, monty_s, file) == 1)
+		if (executeop(stack,input, line, file) == 1)
 		{
 			free(line);
 			fclose(file);
 			exit(EXIT_FAILURE);
 		}
-		line_count++;
+		input++;
 	}
 	free(line);
 	_fdlist(stack);
 	fclose(file);
-	return (0):
+	return (0);
 }
 
 
